b1184b38ac2dcab4b1b12bf06b2910a39e174c35
Jack Tinker <jackrtinker@gmail.com>
2025-02-03 14:58:57 -0500

    Added functionality to move the image

--- i-did-that.js
+++ i-did-that.js
@@ -20,8 +20,25 @@ trumpPic.onload = function () {
 
     //setting position (bottom left) based on image size
     trumpPic.style.bottom = "0px"
-    trumpPic.style.left = trumpPic.offsetWidth + "px"
+    trumpPic.style.left = "0px"
     
+    //adding clickable functionality
+    trumpPic.onclick = function() {
+        moveImage(trumpPic)
+    };
+
     //add to image
     document.body.appendChild(trumpPic)
+};
+
+//moves image to left/right side
+function moveImage (img) {
+    if (img.style.left != ""){
+        img.style.left = ""
+        img.style.right = "0px"
+    }
+    else{
+        img.style.right = ""
+        img.style.left = "0px"
+    }
 };
